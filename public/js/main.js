1a9de6ae41d92de6745f0f5d08e653736bfab895
山本 渚生 <shotayamada7@gmail.com>
2020-08-22 17:36:34 +0900

    commit

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+(function(){
+ 'use strict';
+
+  let cmds = document.getElementsByClassName('del');
+  let i;
+
+  for (i = 0; i < cmds.length; i++) {
+    cmds[i].addEventListener('click', function(e){
+      e.preventDefault();
+      if (confirm('are you sure?')) {
+        document.getElementById('form_' + this.dataset.id).submit();
+      }
+    });
+    
+  }
+
+
+
+})();
